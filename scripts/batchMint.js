e011f1e9cf1cccd78c06aff9608ffaede75043ea
nakulhgr1 <nakulsharmadewf@gmail.com>
2024-07-21 00:12:55 +0530

    Create batchMint.js

---
+++ scripts/batchMint.js
@@ -0,0 +1,40 @@
+// This script batch mints criceket ERC721A tokens.
+
+// Import required libraries
+const { ethers } = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  // Get private key from env
+  const privateKey = process.env.MyKey;
+
+  // The URL of the network provider
+  const networkAddress =
+    "https://rpc.sepolia.ethpandaops.io";
+
+  // Create a provider using the URL
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
+
+  // Create a signer from the private key and provider
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  // Tthe address of the deployed contract
+  const contractAddress = "0xC55d9451d6256D93a6BB2430b5025e7AC3B3C82C";
+
+  // Get the contract factory and attach it to the signer
+  const IndianNFT = await ethers.getContractFactory("car", signer);
+  const contract = await IndianNFT.attach(contractAddress);
+
+  // Call the mint function on the contract to mint 5 tokens
+  await contract.mint(5);
+
+  // Log a message to the console to indicate that the tokens have been minted
+  console.log("Minted 5 tokens");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
